23460503622e9ae248ca871ed204236ec128824b
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2022-09-04 18:07:40 +0200

    Update two-sum.java

--- two-sum.java
+++ two-sum.java
@@ -16,7 +16,7 @@ class Solution {
                 return new int[] {num_map.get(complement),i};
                 
             }
-            
+            //we want to iterate over already existing key->values. If for example, this line before goes on top of if statement, for {5,3,4},10, the (wrong result would be [0,0]
             num_map.put(nums[i],i);
              
         }
