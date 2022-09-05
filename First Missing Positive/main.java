9c7aa8cd4c8749c56710e9ca92bdcf771d0b103a
Fran Pérez <78259026+franpersanchez@users.noreply.github.com>
2022-09-05 21:11:44 +0200

    Create main.java

---
+++ First Missing Positive/main.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+// not the most eficient solution Runtime: 130 ms, faster than 5.04% of Java online submissions for First Missing Positive.
+//Memory Usage: 102.6 MB, less than 67.36% of Java online submissions for First Missing Positive.
+
+public class Main {
+    public static void main(String[] args) {
+        Solution ejercicio = new Solution();
+        int[] array1= {-3,-1,1,2,3,4,5};
+
+        System.out.println(ejercicio.firstMissingPositive(array1));
+    }
+}
+    //Given an unsorted integer array nums, return the smallest missing positive integer.
+    //You must implement an algorithm that runs in O(n) time and uses constant extra space.
+ class Solution{
+//this can be resolved either with Java 8 Stream API. "Stream" coge todos los parámetros y permite añadirle atributos como
+        //filter, sort...etc.
+        //la otra forma es mediante la conversión a Set. A Set is a Collection that contains no duplicates.
+        public int firstMissingPositive(int[] nums) {
+            //creamos un Set Collection a partir del Array. Distinct para decir que no haya repetidos. boxed para pasar las
+            //variables como Integer no como int. "Integer wraps a value of the primitive type int in an object".
+            //toSet collector is used for collecting all Stream elements into a Set instance
+            Set<Integer> setOfUniques = Arrays.stream(nums).distinct().boxed().collect(Collectors.toSet());
+
+            System.out.println(setOfUniques.toString());
+
+            int index=1;
+
+            while(true) {
+                if(!setOfUniques.contains(index)){
+                    return index;
+                }
+                index++;
+            }
+
+        }
+
+}
